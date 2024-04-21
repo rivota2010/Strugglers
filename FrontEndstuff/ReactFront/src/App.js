975581900b46e5ceaf9b0d1b346a4fe1e02ade36
rivota2010 <114026101+rivota2010@users.noreply.github.com>
2024-04-20 22:28:29 -0500

    Update App.js
    Added Styling import App.css and removed "Lines" text

--- FrontEndstuff/ReactFront/src/App.js
+++ FrontEndstuff/ReactFront/src/App.js
@@ -1,11 +1,12 @@
 // App.js
 import React from "react";
 import SignUp from "./components/SignUp";
+import './App.css'; 
 
 function App() {
   return (
     <div className="App">
-      <h1>Lines</h1>
+      <h1></h1>
       <SignUp />
     </div>
   );
