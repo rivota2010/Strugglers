9a92a8bdd0cf8ec2b734132e809bd977bb15ce03
JuanchoDluffy <66972166+JuanchoDluffy@users.noreply.github.com>
2024-05-05 22:45:49 -0600

    fixed routes to components

--- FrontEndstuff/ReactFront/src/App.js
+++ FrontEndstuff/ReactFront/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import Login from "./components/LogIn";
 import MessagingApp from "./components/main";
-
+import Landing from "./components/Landing";
 function App() {
   return (
     <Router>
@@ -16,6 +16,8 @@ function App() {
           {/* Route for Login component */}
           <Route path="/main" element={<MessagingApp />} />{" "}
           {/* Route for MessagingApp component */}
+          <Route path="/Landing" element={<Landing />} />{" "}
+          {/* Route for Landing component */}
         </Routes>
       </div>
     </Router>
